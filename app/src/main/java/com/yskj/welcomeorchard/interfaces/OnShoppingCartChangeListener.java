d467b6f666e9fa1d372e00a509f353557fb321e0
P-V-10\Administrator <1075759633@qq.com>
2017-12-15 09:05:17 +0800

    删除项目

--- app/src/main/java/com/yskj/welcomeorchard/interfaces/OnShoppingCartChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.yskj.welcomeorchard.interfaces;
-
-/**
- * Created by Administrator on 2015/11/3.
- */
-public interface OnShoppingCartChangeListener {
-    void onDataChange(String selectCount, String selectMoney);
-    void onSelectItem(boolean isSelectedAll);
-}
